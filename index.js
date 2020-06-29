52f49defbc2d442db804315b0d4a22562cafd1d4
Jacob Reilly-Cooper <jacob.reilly.cooper@gmail.com>
2020-06-30 00:29:41 +0100

    Code cleaned up. Correct info needed.

--- index.js
+++ index.js
@@ -4,44 +4,15 @@ const sql = require('mssql');
 
 app.get('/', async (req, res) => {
 
-    const config = {
-        user: 'sa',
-        password: 'nicetry',
-        server: 'localhost',
-        database: 'Data'
-    }
-
-    sql.connect(config, function (err) {
-    
-        if (err) console.log(err);
-
-        // create Request object
-        const request = new sql.Request();
-           
-        // query to the database and get the records
-        request.query('SELECT * FROM', function (err, recordset) {
-            
-            if (err) console.log(err)
-
-            // send records as a response
-            res.send(recordset);
-            
-        });
-    });
-
-    /*
-
     try {
         // make sure that any items are correctly URL encoded in the connection string
-        await sql.connect('mssql://username:password@localhost/database')
+        await sql.connect('mssql://sa:password@localhost/database')
         const result = await sql.query`select * from mytable where id = ${value}`
         console.dir(result)
     } catch (err) {
         // ... error checks
     }
-
-    */
-
+    //* Code is cleaner, just needs the correct information.
     res.send("Hello, World. I am trying to find something interesting to do.");
 })
 
