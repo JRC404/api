8ddfb54122d1d828baf39b53e6255ed1f0dea092
Jacob Reilly-Cooper <jacob.reilly.cooper@gmail.com>
2020-06-29 23:47:20 +0100

    mssql included. Code added to index.js

--- index.js
+++ index.js
@@ -1,7 +1,34 @@
 const express = require('express');
 const app = express();
+const sql = require('mssql');
 
 app.get('/', async (req, res) => {
+
+    const config = {
+        user: 'sa',
+        password: 'nicetry',
+        server: 'localhost',
+        database: 'Data'
+    }
+
+    sql.connect(config, function (err) {
+    
+        if (err) console.log(err);
+
+        // create Request object
+        const request = new sql.Request();
+           
+        // query to the database and get the records
+        request.query('SELECT * FROM', function (err, recordset) {
+            
+            if (err) console.log(err)
+
+            // send records as a response
+            res.send(recordset);
+            
+        });
+    });
+
     res.send("Hello, World. I am trying to find something interesting to do.");
 })
 
